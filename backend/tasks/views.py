589755eb8bfb5827dec11002aabec49d51ce1eb9
Anouk udu <anoukudumalagala@gmail.com>
2026-03-26 14:58:06 +0530

    Fix

--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -3,9 +3,9 @@ from rest_framework import viewsets
 from .models import Task
 from .serializers import TaskSerializer
 
-from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
-    parser_classes = (MultiPartParser, FormParser)
+    parser_classes = (MultiPartParser, FormParser, JSONParser)

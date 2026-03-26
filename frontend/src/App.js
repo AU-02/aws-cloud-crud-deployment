f2d86aa06d5cbb3a323d193d25197221276b945f
Anouk udu <anoukudumalagala@gmail.com>
2026-03-26 12:16:14 +0530

    Add React frontend with basic crud operations

---
+++ frontend/src/App.js
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+  const [title, setTitle] = useState("");
+
+  const fetchTasks = async () => {
+    const res = await axios.get("/api/tasks/");
+    setTasks(res.data);
+  };
+
+  const addTask = async () => {
+    await axios.post("/api/tasks/", { title });
+    fetchTasks();
+  };
+
+  const deleteTask = async (id) => {
+    await axios.delete(`/api/tasks/${id}/`);
+    fetchTasks();
+  };
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  return (
+    <div>
+      <h1>Tasks</h1>
+      <input value={title} onChange={(e) => setTitle(e.target.value)} />
+      <button onClick={addTask}>Add</button>
+
+      <ul>
+        {tasks.map(t => (
+          <li key={t.id}>
+            {t.title}
+            <button onClick={() => deleteTask(t.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
